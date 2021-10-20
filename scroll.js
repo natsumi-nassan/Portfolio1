b3d2fca00957608e183a90963ada256c8a56efd5
natsumi-nassan <92068277+natsumi-nassan@users.noreply.github.com>
2021-10-20 15:14:25 +0900

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function(){
+$('#animation').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ });
+});
+});
